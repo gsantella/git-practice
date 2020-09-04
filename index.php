d86e96a181021a0a6c06a039f8ec804f9e5b943f
Guido Santella <gsantella@southhills.edu>
2020-09-04 14:49:08 -0400

    basic setup of separation

---
+++ index.php
@@ -0,0 +1,29 @@
+<h1>Hello World!</h1>
+
+<hr/>
+
+<?php include('includes/ben.php') ?>
+
+<hr/>
+
+<?php include('includes/caleb.php') ?>
+
+<hr/>
+
+<?php include('includes/david.php') ?>
+
+<hr/>
+
+<?php include('includes/gavin.php') ?>
+
+<hr/>
+
+<?php include('includes/guido.php') ?>
+
+<hr/>
+
+<?php include('includes/sam.php') ?>
+
+<hr/>
+
+<?php include('includes/scott.php') ?>
