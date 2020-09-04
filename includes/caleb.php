2b6339a8ffde30b297e4c27c1ea91356f86e7c00
cmorningstar <70662476+cmorningstar@users.noreply.github.com>
2020-09-04 15:04:40 -0400

    Update caleb.php

--- includes/caleb.php
+++ includes/caleb.php
@@ -1,7 +1,7 @@
 <?php 
-    $red = rand(10, 245)
-    $green = rand(10, 245)
-    $blue = rand(10, 245)
+    $red = rand(10, 245):
+    $green = rand(10, 245):
+    $blue = rand(10, 245):
 ?>
 <p style="color:rgb(<?=$red?>, <?=$green?>, <?=$blue?>)">Caleb Morningstar, testing php uploads</p>
 <p>Click </p>
