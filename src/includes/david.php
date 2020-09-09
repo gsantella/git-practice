8dd841aaf23f2cfcf5c871aa9c40b003e0632bff
dlaff666 <70662517+dlaff666@users.noreply.github.com>
2020-09-09 14:47:40 -0400

    Created dynamic bold
    "David Lafferty" will now randomly alternate between bold and normal.

--- src/includes/david.php
+++ src/includes/david.php
@@ -1,1 +1,15 @@
-<p style="background-color:#a05050">This section is STILL under construction by David Lafferty</p>
+<?php
+    $bold = "";
+    $rand = rand(0,1);
+    if ($rand == 0)
+    {
+        $bold = "bold";
+    }
+    else
+    {
+        $bold = "normal";
+    }
+?>
+<p style="background-color:#a05050">This section is STILL under construction by
+    <span style='font-weight:<?= $bold ?>'>David Lafferty</span>
+</p>
