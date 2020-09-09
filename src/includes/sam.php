7f7a6c18f61e66b4565c7829f8705d4eb37c67f6
TNTBlasterZapper <LordPenguin6@yahoo.com>
2020-09-09 15:14:16 -0400

    Added link to full story in sam.php

--- src/includes/sam.php
+++ src/includes/sam.php
@@ -2,3 +2,4 @@
 <p>Welcome to the news, Today is <?= date('l') ?></p>
 <p>It's a lovely day today.</p>
 <p>, and now Scott with the weather.</p>
+<h3>Click <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">HERE</a> for full story</h3>
