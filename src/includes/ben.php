53f63fac18cab43ddc2c3529571a303a647c0281
bduclo22 <70662467+bduclo22@users.noreply.github.com>
2020-09-09 14:27:50 -0400

    Update ben.php

--- src/includes/ben.php
+++ src/includes/ben.php
@@ -1,4 +1,4 @@
 <h1>Tacos are great!</h1>
 <p>Tacos are the best food.</p>
 <!--<img src="https://images-gmi-pmc.edge-generalmills.com/e59f255c-7498-4b84-9c9d-e578bf5d88fc.jpg" alt="Taco">-->
-<p>Hi</p>
+<p>Goodbye taco image :(</p>
