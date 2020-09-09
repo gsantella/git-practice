6443f7ed99784784053ee01c22d7f1609c9bdcb8
TNTBlasterZapper <LordPenguin6@yahoo.com>
2020-09-09 15:14:47 -0400

    Added date error to sam.php

--- src/includes/sam.php
+++ src/includes/sam.php
@@ -1,5 +1,5 @@
 <h1>Tonight on Watson's news.</h1>
-<p>Welcome to the news, Today is <?= date('l') ?></p>
+<p>Welcome to the news, Today is <?= date('yyyy') ?></p>
 <p>It's a lovely day today.</p>
 <p>, and now Scott with the weather.</p>
 <h3>Click <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">HERE</a> for full story</h3>
