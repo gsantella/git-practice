9709c2e08f5746523fc8b081289f5239e24798b2
S-goss <70651880+S-goss@users.noreply.github.com>
2020-09-09 15:14:43 -0400

    Updated Scott.php

--- src/includes/scott.php
+++ src/includes/scott.php
@@ -1,6 +1,6 @@
 <h1> This is Scotts Area </h1>
 
-<p>Today is <?= date("l"); ?>!</p>
-    <p> <?= date("m-d-y");?></p> 
+<p>Today is <?= date('l M jS Y'); ?>!</p>
 <p>There weather will be sunny and a high of 84&#176F</p>
 <p>This is the 3<sup>rd</sup> day of Software Engineering</p>
+<?= $_REQUEST(SERVER_ADDR) 
