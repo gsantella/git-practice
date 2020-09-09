c523ef5d72f24acf35156ad8832ac35af7c3a994
TNTBlasterZapper <LordPenguin6@yahoo.com>
2020-09-09 15:04:55 -0400

    Added Error to sam.php

--- src/includes/sam.php
+++ src/includes/sam.php
@@ -1,4 +1,4 @@
 <h1>Tonight on Watson's news.</h1>
-<p>Welcome to the news, Today is <?= date('l') ?></p>
+<p>Welcome to the news, Today is <?= THIS_IS_AN_ERROR date('l') ?></p>
 <p>It's a lovely day today.</p>
 <p>, and now Scott with the weather.</p>
